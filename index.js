2545b04af9b237b0e2b9660ff6697ed3045c91bb
Pauloper1 <paulcrescent.p@gmail.com>
2023-02-05 05:19:02 +0530

    index.js cleaned

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/Dashboard.html", (req,res)=>{
 });
 
 app.get("/pages/HTML/underScrutiny.html", (req,res)=>{
-    res.sendFile(__dirname + "/pages/HTML/underScrutiny.html");
+    res.sendFile(__dirname + "/pages/HTML/underScrutiny.html")});
 app.get("/pages/Dashboard.html", (req,res)=>{
     res.sendFile(__dirname + "/pages/HTML/Dashboard.html");
 });
@@ -44,10 +44,6 @@ app.get("/pages/user_form.html", (req,res)=>{
 });
 
 
-const BUser = {
-    email:"BU@gamil.com",
-    passwd:"1234"
-}
 app.post("/auth",(req,res)=>{
     console.log(req.body)
     if(req.body.email === BUser.email && req.body.passwd === BUser.passwd){
@@ -59,43 +55,7 @@ app.post("/auth",(req,res)=>{
     }
 })
 
-app.post('/webhook', (req, res) => {
-    // get agent from request
-    let agent = new WebhookClient({request: req, response: res})    // create intentMap for handle intent
-    let intentMap = new Map();    // add intent map 2nd parameter pass function
-    intentMap.set('Temp',handleWebHookIntent)    // now agent is handle request and pass intent map
-    agent.handleRequest(intentMap)
-})
-function handleWebHookIntent(agent){
-    agent.add("Hello I am Webhook demo How are you...")
-}
 
 app.listen(5000, ()=>{
     console.log("Server started Running!");
-});
-
-
-
-
-// New
-// mongoose.connect('mongodb://localhost:5000/testdb',{userNewUrlParser: true, unifiedTopology:true})
-// const db= mongoose.connection
-//  db.on('error',(err) => {
-//     console.log(err)
-//  })
-
-//  db.once('open', ()=>{
-//     console.log('Database connecttion done')
-//  }) 
-
-// const app = express()
-
-// app.use(morgan('dev'))
-// app.use(bodyParser.urlencoded({extended:true}))
-// app.use(bodyParser.json())
-
-// const PORT = process.env.PORT || 3000
-
-// app.listen(PORT , ()=>{
-//     console.log('Server running on port ${PORT} ')
-// })
+})
