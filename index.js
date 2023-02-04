6dbfbe5459d99ee3f165e810d75552d5ac79a59e
justin mathews <justin.mathews29@gmail.com>
2023-02-05 02:18:49 +0530

    modified indexjs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
-// const hbs=require("hbs");
-
 const bodyParser = require("body-parser");
+const mongoose=require("mongoose");
+const morgan=require("morgan");
+
 
 const app = express(); 
 
@@ -12,8 +12,55 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + "\\pages\\HTML\\signin.html");
 });
 
+app.get("/signup.html", (req,res)=>{
+    res.sendFile(__dirname + "\\pages\\HTML\\signup.html");
+});
+
+app.get("/Dashboard.html", (req,res)=>{
+    res.sendFile(__dirname + "\\pages\\HTML\\Dashboard.html");
+});
+
+app.get("/pages/Dashboard.html", (req,res)=>{
+    res.sendFile(__dirname + "\\pages\\HTML\\Dashboard.html");
+});
+
+
+app.get("/pages/needs.html", (req,res)=>{
+    res.sendFile(__dirname + "\\pages\\HTML\\needs.html");
+});
+
+app.get("/pages/user_form.html", (req,res)=>{
+    res.sendFile(__dirname + "\\pages\\HTML\\user_form.html");
+});
+
 
 
 app.listen(5000, ()=>{
     console.log("Server started Running!");
 });
+
+
+
+
+// New
+// mongoose.connect('mongodb://localhost:5000/testdb',{userNewUrlParser: true, unifiedTopology:true})
+// const db= mongoose.connection
+//  db.on('error',(err) => {
+//     console.log(err)
+//  })
+
+//  db.once('open', ()=>{
+//     console.log('Database connecttion done')
+//  }) 
+
+// const app = express()
+
+// app.use(morgan('dev'))
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json())
+
+// const PORT = process.env.PORT || 3000
+
+// app.listen(PORT , ()=>{
+//     console.log('Server running on port ${PORT} ')
+// })
